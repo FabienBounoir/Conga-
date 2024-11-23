3b7dbff714dd443a221a4af485f9fb1b5daec032
FabienBounoir <bounoirfabien@gmail.com>
2024-11-23 09:40:47 +0100

    feat: 🎸 Update svelte config

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,12 @@
-import vercel from '@sveltejs/adapter-vercel';
-import preprocess from "svelte-preprocess";
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+	preprocess: vitePreprocess(),
 	kit: {
-		adapter: vercel({
-			runtime: 'nodejs18.x' // or 'nodejs20.x'
-		})
+		adapter: adapter()
 	}
 };
 
 export default config;
-
-
