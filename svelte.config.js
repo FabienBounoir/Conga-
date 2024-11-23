5b37d9bbc9f7d55a8fd644a609fa4dd2d2d3ae66
FabienBounoir <bounoirfabien@gmail.com>
2024-11-23 09:24:24 +0100

    fix: 🐛 PLEASE WORKKK

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,16 @@
-import adapter from '@sveltejs/adapter-auto';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: preprocess(),
 	kit: {
-		adapter: adapter()
+		adapter: vercel({
+			runtime: 'nodejs18.x' // or 'nodejs20.x'
+		})
 	}
 };
 
 export default config;
+
+
